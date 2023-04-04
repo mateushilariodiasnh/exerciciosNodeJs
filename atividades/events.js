a74482f5202e9303c3868525be0135731e72d600
Mateus Hilário Dias <mateus.dias@novahaus.com.br>
2023-04-04 14:11:23 -0300

    added files

---
+++ atividades/events.js
@@ -0,0 +1,8 @@
+const {EventEmitter} = require('events')
+const events = new EventEmitter()
+
+events.on('saySomething', (message) => {
+    console.log('Eu esperava mais kkkk', message)
+})
+/* events.once ouve apenas uma vez */
+events.emit('saySomething', "senhor 
